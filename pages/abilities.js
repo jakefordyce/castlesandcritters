39c9195d4ac02755707f6e957e0866222ea738df
jakefordyce <jakefordyce78@gmail.com>
2022-08-12 12:42:26 -0400

    add more ability info and move abilities into their own page

--- pages/abilities.js
+++ pages/abilities.js
@@ -1,11 +1,19 @@
 import styles from '../styles/Home.module.css'
 import { abilities } from '../data/abilities'
 
-export default function Home({abilities}) {
+export default function Abilities({abilities}) {
 
+  const abilityDivs = abilities.map((ability, index) => 
+    <div key={index} className="ability">
+      <p className='name'>{ability.name}</p>
+      <p>{ability.text}</p>
+    </div>
+  );
 
   return (
     <div className={styles.container}>
+
+      {abilityDivs}
       
     </div>
   )
